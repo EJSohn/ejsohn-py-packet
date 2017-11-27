72c590a759fa2827c1b7407f0d0e4c429254da0a
EJSohn <5on.3un.7oo@gmail.com>
2017-11-27 17:45:55 +0000

    [ADD] Get IPv4(TCP/UDP) data

--- ejpypacket.py
+++ ejpypacket.py
@@ -2,6 +2,10 @@ import socket,struct,binascii,os
 from general import *
 from pcap import Pcap
 from ethernet import Ethernet
+from ipv4 import IPv4
+from tcp import TCP
+from http import HTTP
+from udp import UDP
 
 if os.name == "nt":
     s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
@@ -31,6 +35,52 @@ while True:
     print('\nEthernet Frame:')
     print(TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac, eth.src_mac,
     eth.proto))
+
+    # IPv4
+    if eth.proto == 8:
+        ipv4 = IPv4(eth.data)
+        print(TAB_1 + 'IPv4 Packet:')
+        print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version,
+        ipv4.header_length, ipv4.ttl))
+        print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src,
+        ipv4.target))
+        
+        # TCP
+        if ipv4.proto == 6:
+            tcp = TCP(ipv4.data)
+            print(TAB_1 + 'TCP Segment:')
+            print(TAB_2 + 'Source Port: {}, Destination Port: {}'.format(tcp.src_port,
+            tcp.dest_port))
+            print(TAB_2 + 'Sequence: {}, Acknowledgment: {}'.format(tcp.sequence,
+            tcp.acknowledgment))
+            print(TAB_2 + 'Flags:')
+            print(TAB_3 + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg, tcp.flag_ack,
+            tcp.flag_psh))
+            print(TAB_3 + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst, tcp.flag_syn,
+            tcp.flag_fin))
+
+            if len(tcp.data) > 0 :
+                # HTTP
+                if tcp.src_port == 80 or tcp.dest_port == 80:
+                    print(TAB_2 + 'HTTP Data:')
+                    try:
+                        http = HTTP(tcp.data)
+                        http_info = str(http.data).split('\n')
+                        for line in http_info:
+                            print(DATA_TAB_3 + str(line))
+                    except:
+                        print(format_multi_line(DATA_TAB_3, tcp.data))
+                else:
+                    print(TAB_2 + 'TCP Data:')
+                    print(format_multi_line(DATA_TAB_3, tcp.data))
+        elif ipv4.proto == 17:
+            udp = UDP(ipv4.data)
+            print(TAB_1 + 'UDP Segment:')
+            print(TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(udp.src_port,
+            udp.dest_port, udp.size))
+
+pcap.close()
+
 
 
     
